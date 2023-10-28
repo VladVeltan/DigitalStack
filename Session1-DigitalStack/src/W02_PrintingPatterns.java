4070f7ab074bb28861ad4921080894db58751010
VladVeltan <91504476+VladVeltan@users.noreply.github.com>
2023-10-28 12:16:20 +0300

    "Printing pattern problems"

---
+++ Session1-DigitalStack/src/W02_PrintingPatterns.java
@@ -0,0 +1,32 @@
+public class W02_PrintingPatterns {
+
+    public static void trianglePattern1(int n) {
+        int count=0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                count++;
+                System.out.print(count+ " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printPascalTriangle(int n)
+    {
+        for(int i=1;i<=n;i++)
+        {   int currentValue=1;
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(currentValue+" ");
+                currentValue=currentValue*(i-j)/j;
+            }
+            System.out.println();
+        }
+
+
+    }
+    public static void main(String[] args) {
+        //trianglePattern1(5);
+        printPascalTriangle(6);
+    }
+}
